5fd91e18000604c632556754b8c8a8f366ab79af
Johannes Lumpe <johannes@johanneslumpe.de>
2019-05-17 13:01:32 -0400

    add failing tests

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,26 @@
-describe('page', () => {
-  it('works', () => {})
-})
+describe('sticky elements', () => {
+  describe('issue 565/571 with updated fixture', () => {
+    before(() => {
+      cy
+        .viewport(400, 400)
+        .visit("cypress/fixtures/issue-565-updated.html");
+    });
+
+    it('can click the first tr', () => {
+      cy.get("td:first").click()
+    });
+  });
+
+  describe('additional example with side panel', () => {
+    before(() => {
+      cy
+        .viewport(400, 400)
+        .visit("cypress/fixtures/sidepanel.html");
+    });
+
+    it('can click the first tr', () => {
+      cy.get("td:first").click()
+    });
+  });
+
+});
